da358fccd68c192c4c379f6cb3803b2d11121a79
Thalis Sousa <tatagamer677@gmail.com>
2026-03-21 23:43:07 -0300

    nova tela

---
+++ app/src/main/java/br/com/fiap/recipes/screens/SignupScreen.kt
@@ -0,0 +1,279 @@
+package br.com.fiap.recipes.screens
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.recipes.R
+import br.com.fiap.recipes.ui.theme.RecipesTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.RemoveRedEye
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.TextField
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+
+
+// *** Tela SignupScreen ***
+@Composable
+fun SignupScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ){
+        TopEndCard(modifier = Modifier.align(Alignment.TopEnd))
+        BottomStartCard(modifier = Modifier.align(Alignment.BottomStart))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleComponent()
+            Spacer(modifier = Modifier.height(48.dp))
+            UserImage()
+            SignupUserForm()
+        }
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    locale = "en"
+)
+@Composable
+private fun SignupScreenPreview() {
+    RecipesTheme {
+        SignupScreen()
+    }
+
+}
+
+@Composable
+fun TitleComponent(modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text= stringResource(R.string.sign_up),
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text= stringResource(R.string.create_a_new_account),
+            color= MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleSmall
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TitleComponentPreview() {
+    RecipesTheme {
+        TitleComponent()
+    }
+    
+}
+
+@Composable
+fun UserImage(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .size(120.dp)
+    ) {
+        Image (
+            painter = painterResource(R.drawable.user),
+            contentDescription = stringResource(R.string.user_image),
+            modifier = Modifier
+                .size(110.dp)
+                .align(alignment = Alignment.Center)
+        )
+        Icon(
+            imageVector = Icons.Default.PhotoCamera,
+            contentDescription = stringResource(R.string.camera_icon),
+            // tint aplica cor a icons
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+            .align(alignment = Alignment.BottomEnd)
+        )
+    }
+    
+}
+
+@Preview
+@Composable
+private fun UserImagePreview() {
+    RecipesTheme {
+        UserImage()
+    }
+    
+}
+
+@Composable
+fun SignupUserForm(modifier: Modifier = Modifier) {
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .padding(32.dp))
+    {
+    // Caixa de texto para nome do usuario
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp),
+            label = {
+                Text(
+                    text = stringResource(R.string.your_name),
+                    style = MaterialTheme.typography.labelSmall
+                )
+            },
+            // podemos fazer tambem shape = RoundedCornerShape(topEnd = 16.dp), dando curvatura só em um canto
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors (
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.primary
+
+            ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = stringResource(R.string.person_icon),
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+
+        )
+
+        // Caixa de texto para email do usuario
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp),
+            label = {
+                Text(
+                    text = stringResource(R.string.your_e_mail),
+                    style = MaterialTheme.typography.labelSmall
+                )
+            },
+            // podemos fazer tambem shape = RoundedCornerShape(topEnd = 16.dp), dando curvatura só em um canto
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors (
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.primary
+
+            ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Mail,
+                    contentDescription = stringResource(R.string.mail_icon),
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+
+        )
+
+        // Caixa de texto para senha do usuario
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp),
+            label = {
+                Text(
+                    text = stringResource(R.string.your_password),
+                    style = MaterialTheme.typography.labelSmall
+                )
+            },
+            // podemos fazer tambem shape = RoundedCornerShape(topEnd = 16.dp), dando curvatura só em um canto
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors (
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.primary
+
+            ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = stringResource(R.string.lock_icon),
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.RemoveRedEye,
+                    contentDescription = stringResource(R.string.remove_red_eye),
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+                           },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType
+                    .NumberPassword,
+                imeAction = ImeAction.Done
+            )
+        ) // Botão Create account
+         Spacer(
+             modifier = Modifier
+                 .height(32.dp)
+         )
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+        shape = RoundedCornerShape(8.dp)
+         )
+        {
+            Text(
+                text = stringResource(R.string.create_account),
+                style = MaterialTheme.typography.labelMedium )
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+private fun SignupUserFormPreview() {
+    RecipesTheme {
+        SignupUserForm()
+    }
+    
+}
